4db22966fdb432a5ff2cb050cb5b32468fe9b636
Duncan Brown <duncan.brown@dxw.com>
2016-11-10 17:12:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//Lets require/import the HTTP module
+var http = require('http');
+
+const PORT=8080;
+
+function handleRequest(request, response){
+    console.log("Request received");
+    setTimeout(function() {
+      console.log('Request served');
+      response.end('Hello World');
+    }, 300);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
